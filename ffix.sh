91acce42a3909d6a510d4b74c4e8d97daf527d0a
timrcoulson <tim@justpark.com>
2016-08-24 01:22:01 +0200

    Tidy up

--- ffix.sh
+++ ffix.sh
@@ -13,7 +13,18 @@ echo "Please enter the name of your base branch: "
 read BRANCH
 
 cp rules_template.xml /tmp/rules.xml
-curl 'https://api.github.com/repos/${OWNER}/${REPO}/pulls?access_token=${API_TOKEN}' | grep "\"ref\": \"" | grep -v $BRANCH | sed 's/.*"ref": "//g' | sed 's/",//g' | xargs -n 1 git diff --name-only $BRANCH -0 | awk '!seen[$0]++' | sed 's/\(.*\)/<exclude-pattern>\1<\/exclude-pattern>/g' >> /tmp/rules.xml 
+
+echo "Fetching files to exclude from code style sweep"
+curl 'https://api.github.com/repos/${OWNER}/${REPO}/pulls?access_token=${API_TOKEN}' | 
+grep "\"ref\": \"" |
+grep -v $BRANCH |
+sed 's/.*"ref": "//g' |
+sed 's/",//g' |
+xargs -n 1 git diff --name-only $BRANCH -0 |
+awk '!seen[$0]++' |
+sed 's/\(.*\)/<exclude-pattern>\1<\/exclude-pattern>/g' >> /tmp/rules.xml 
+
 echo -n '</ruleset>' >> /tmp/rules.xml
 
+echo "Running code style fixer"
 phpcbf --standard=/tmp/rules.xml ./public/utils 
